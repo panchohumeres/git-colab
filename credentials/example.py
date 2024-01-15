14468dc513a677858ef26aef9ee2c420c5da26ce
Francisco Humeres Marfan <fhumeres@alum.mit.edu>
2024-01-14 23:03:39 -0300

    Update example.py

--- credentials/example.py
+++ credentials/example.py
@@ -6,3 +6,6 @@ email="your_git_email@your_mail_host.com"
 
 #Access token (if any)
 pat="your_token_12345"
+
+#folder in google drive where local copy of repo will be hosted
+path="/your/path/in/drive"
